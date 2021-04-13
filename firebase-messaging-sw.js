4b35aceed7cedf655a7c65fa0a6dde6dea029be8
Philx0 <32342766+Philx0@users.noreply.github.com>
2021-04-13 15:16:05 +0300

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,25 @@
+importScripts('https://www.gstatic.com/firebasejs/7.14.6/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/7.14.6/firebase-messaging.js');
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAvnJjYWu4N11Fw8HL7v8JYPwWJjlI5JNA",
+  authDomain: "droidautomate.firebaseapp.com",
+  projectId: "droidautomate",
+  storageBucket: "droidautomate.appspot.com",
+  messagingSenderId: "111193523408",
+  appId: "1:111193523408:web:bc5070ec3c82ee36170ad5",
+  measurementId: "G-RBVHX221KR"
+};
+
+firebase.initializeApp(firebaseConfig);
+const messaging=firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function (payload) {
+    console.log(payload);
+    const notification=JSON.parse(payload);
+    const notificationOption={
+        body:notification.body,
+        icon:notification.icon
+    };
+    return self.registration.showNotification(payload.notification.title,notificationOption);
+});
